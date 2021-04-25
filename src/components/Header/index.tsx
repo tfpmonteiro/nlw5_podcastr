3ca90af5643bb60f3764f7d8d192bc6198fc7767
Talita Monteiro <talis_phn2@hotmail.com>
2021-04-25 16:51:28 -0300

    aula 3 - criação da pagina home com a listagem de podcasts e aprendizado sobre SSG

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link'
 
 import styles from './styles.module.scss'
 
 export function Header() {
-const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-  locale: ptBR,
-})
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  })
 
-  return(
+  return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcaster"/>
+      <Link href="/">
+        <img src="/logo.svg" alt="Podcaster" />
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
